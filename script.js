9441f739d4a0662ff11e014873a8af0fbcbcec5e
Sifat Anindho <sifatulislam0509@gmail.com>
2025-03-14 19:44:15 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let video = document.getElementById("video");
+let videoInput = document.getElementById("videoInput");
+let popup = document.getElementById("popup");
+let surveyForm = document.getElementById("surveyForm");
+let reportData = [];
+videoInput.addEventListener("change", function(event) {
+    let file = event.target.files[0];
+    if (file) {
+        let url = URL.createObjectURL(file);
+        video.src = url;
+        video.style.display = "block";
+        video.currentTime = 0;
+        reportData = [];
+        setTimeout(checkPause, 60000);
+    }
+});
+function checkPause() {
+    if (!video.paused && video.currentTime > 0 && (Math.floor(video.currentTime) % 60 === 0 || video.currentTime >= video.duration - 1)) {
+        video.pause();
+        popup.style.display = "flex";
+    }
+    if (!video.ended) {
+        setTimeout(checkPause, 1000);
+    }
+}
+function submitSurvey() {
+    let responses = [];
+    let checkboxes = surveyForm.querySelectorAll("input[type='checkbox']");
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) responses.push(checkbox.value);
+        checkbox.checked = false;
+    });
+    reportData.push(`Time: ${Math.floor(video.currentTime)}s, Responses: ${responses.join(", ")}`);
+    popup.style.display = "none";
+    video.currentTime += 1;
+    video.play();
+}
+function downloadReport() {
+    let blob = new Blob([reportData.join("\n")], { type: "text/plain" });
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "reports.txt";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
